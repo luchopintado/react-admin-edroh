aeff15a3582c988ddd4378c7c9b5c5447293c347
Luis Pintado <luchopintado@gmail.com>
2023-09-29 09:48:50 -0500

    Add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import Topbar from "./scenes/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
-  return <div>App...</div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            App...
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App
